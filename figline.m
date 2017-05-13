c8e4b3bd8c76c599cfe02fe6fdaab6620413a45f
Sanghai Guan <183592@qq.com>
2017-05-13 21:07:25 +0800

    Add 'Color' to style list.

--- figline.m
+++ figline.m
@@ -1,6 +1,6 @@
 function [ ] = figline( LineWidth, MarkerSize, Style)
-%FIGLINE 调整当前绘图中所有曲线线条粗细及数据点类型和大小。
-%        Adjust the LineWidth, LineStyle, MarkerSize and Marker of all the 
+%FIGLINE 调整当前绘图中所有曲线线条颜色，粗细及数据点类型和大小。
+%        Adjust the Color, LineWidth, LineStyle, MarkerSize and Marker of all the 
 %        lines in one step. 
 %
 %   FIGLINE(LineWidth, MarkerSize, Style) 一次设置绘图中全部曲线，
@@ -12,17 +12,18 @@ function [ ] = figline( LineWidth, MarkerSize, Style)
 %   FIGLINE(2, [], '-') 将线宽设置为 2，线形为直线。
 %   FIGLINE(2, 0) 将线宽设置为 2，隐藏数据点。
 %   FIGLINE(0, 6, '.') 隐藏图中曲线，绘制成散点图，散点大小为 6。
-%   FIGLINE('--os') 线宽和数据点大小保持不变，线形设置为虚线，
+%   FIGLINE('--bros') 线宽和数据点大小保持不变，线形设置为虚线，颜色为蓝和红，
 %   数据点形状分别设置为圆和方块，等效于 FIGLINE([], [], '--os')。
 %
 %   For example, FIGLINE(2) set the LineWidth of all the lines as 2.
 %   FIGLINE(2, [], '-') Set the LineWidth as 2 and LineStyle as solid line.
 %   FIGLINE(2, 0) Set the LineWidth as 2 and hide markers.
 %   FIGLINE(0, 6, '.') Hide the line and set MarkerSize as 6.
-%   FIGLINE('--os') Keep the LineWidth and MarkerSize, set the LineStyle as
-%   dashed, and set the two (or more) lines' marker as circle and square 
-%   separately. You can also use FIGLINE([], [], '--os').
+%   FIGLINE('--bros') Keep the LineWidth and MarkerSize, and set the two (or
+%   more) lines' color as blue and red, marker as circle and square 
+%   separately. You can also use FIGLINE([], [], '--bros').
 %
+%   颜色 (Color): 'y' | 'm' | 'c' | 'r' | 'g' | 'b' | 'w' | 'k'
 %   线形 (LineStyle): '-' | '--' | ':' 
 %   数据点形状 (Marker): '+' | 'o' | '*' | '.' | 'x' | 's' | 'd' | '^' | 'v' |'<' | '>' | 'p' | 'h'
 %
@@ -49,12 +50,13 @@ elseif nargin == 2
 end
 
 if ~isempty(Style) && ~isa(Style,'char')
-    error('The Marker is invalid');
+    error('The Style is invalid');
 end
-if ~prod(ismember(Style,'-:+o*.xsd^v<>ph'))
-    error('The Style should be - -- : + o * . x s d ^ v < > p h');
+if ~prod(ismember(Style,'ymcrgbwk-:+o*.xsd^v<>ph'))
+    error('The Style should be y m c r g b w k - -- : + o * . x s d ^ v < > p h');
 end
 
+Color = Style(ismember(Style,'ymcrgbwk'));
 LineStyle = Style(ismember(Style,'-:'));
 Marker = Style(ismember(Style,'+o*.xsd^v<>ph'));
 
@@ -88,9 +90,20 @@ if ~isempty(LineWidth)
 end
 
 if ~isempty(Marker)
+    % you can predefine a markertable here
+    % MarkerTable = repmat(['o' 's' '^' 'v'],1,ceil(length(h)/length(Marker)));
     MarkerTable = repmat(Marker,1,ceil(length(h)/length(Marker)));
     for ii = 1:length(h)
         set(h(ii),'Marker',MarkerTable(ii));
+    end 
+end
+
+if ~isempty(Color)
+    % you can predefine a colortable here
+    % ColorTabel = repmat(['b' 'r' 'm' 'c'],1,ceil(length(h)/length(Color)));
+    ColorTable = repmat(Color,1,ceil(length(h)/length(Color)));
+    for ii = 1:length(h)
+        set(h(ii),'Color',ColorTable(ii));
     end 
 end
 
