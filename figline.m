92c0ce2feb69f46a41dc8a9537b747c204ec8276
Sanghai Guan <183592@qq.com>
2017-05-16 22:28:11 +0800

    Add a customized colormap.

--- figline.m
+++ figline.m
@@ -29,7 +29,11 @@ function [ ] = figline( LineWidth, MarkerSize, Style)
 %   or '- .', other than '-.'(dash-dotted line), to set dotted Marker '.'
 %   and solid line '-'. The same to '--'(dashed line).
 %
-%   颜色 (Color): 'y' | 'm' | 'c' | 'r' | 'g' | 'b' | 'w' | 'k'
+%   函数中提供了 9 个可供自定义的颜色，使用 '1' ~ '9' 调用。
+%   You can customize the color you frequently used in this function, and
+%   call them by '1' to '9'.
+%
+%   颜色 (Color): 'y' | 'm' | 'c' | 'r' | 'g' | 'b' | 'w' | 'k' | '1'~'9'(custom)
 %   线形 (LineStyle): '-' | '--' | ':' | '-.'
 %   数据点形状 (Marker): '+' | 'o' | '*' | '.' | 'x' | 's' | 'd' | '^' | 'v' |'<' | '>' | 'p' | 'h'
 %
@@ -39,6 +43,15 @@ function [ ] = figline( LineWidth, MarkerSize, Style)
 %         plot(x,-y); hold on; plot(x,y+1);
 %         FIGLINE(2, 24, '--.');
 %
+
+% You can predefine colors frequently used here
+ColorMap = {[0 0.45 0.74],[0.85 0.33 0.1],[0.93 0.69 0.13],[0.49 0.18 0.56],...
+    [0.47 0.67 0.19],[0.3 0.75 0.93],[0.64 0.08 0.18],[0.3 0.3 0.3],[0.6 0.6 0.6]};
+
+PermittedColorChar = 'ymcrgbwk';
+PermittedColorNum = '123456789';
+PermittedLineStyle = '-=:;';
+PermittedMarker = '+o*.xsd^v<>ph';
 
 if nargin == 0
     return;
@@ -59,8 +72,8 @@ if ~isempty(Style) && ~isa(Style,'char')
     error('The Style is invalid');
 end
 
-if ~prod(ismember(Style,' ymcrgbwk-:+o*.xsd^v<>ph'))
-    error('The Style should be y m c r g b w k - -- : + o * . x s d ^ v < > p h');
+if ~prod(ismember(Style,[' ' PermittedColorChar PermittedColorNum PermittedLineStyle PermittedMarker]))
+    error('The Style should be y m c r g b w k 1~9 - -- : -. + o * . x s d ^ v < > p h');
 end
 
 if ~isempty(Style)
@@ -68,9 +81,9 @@ if ~isempty(Style)
     Style = strrep(Style,'-.',';');
 end
 
-Color = Style(ismember(Style,'ymcrgbwk'));
-LineStyle = Style(ismember(Style,'-=:;'));
-Marker = Style(ismember(Style,'+o*.xsd^v<>ph'));
+Color = Style(ismember(Style,[PermittedColorChar PermittedColorNum]));
+LineStyle = Style(ismember(Style,PermittedLineStyle));
+Marker = Style(ismember(Style,PermittedMarker));
 
 g = get(gca,'children');
 h = [];
@@ -125,7 +138,11 @@ if ~isempty(Color)
     % ColorTabel = repmat(['b' 'r' 'm' 'c'],1,ceil(length(h)/length(Color)));
     ColorTable = repmat(Color,1,ceil(length(h)/length(Color)));
     for ii = 1:length(h)
-        set(h(ii),'Color',ColorTable(ii));
+        color = ColorTable(ii);
+        if ismember(color,PermittedColorNum)
+            color = ColorMap{str2double(color)};
+        end
+        set(h(ii),'Color',color);
     end 
 end
 
